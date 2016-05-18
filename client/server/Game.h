bc6fd69c749f857b69ef843d7943cb1a270d3532
Eran Yom-Tov <gri120@walla.com>
2016-05-18 20:51:32 +0300

    created most User class functions, and added some of the skeleton for Game class

--- client/server/Game.h
+++ client/server/Game.h
@@ -1,8 +1,12 @@
 #pragma once
 
+class User;
 
 class Game
 {
+public:
+	bool leaveGame(User*);
 private:
 	int _questions_No;
+//	vector<Question*> 
 };
--- client/server/User.cpp
+++ client/server/User.cpp
@@ -10,6 +10,57 @@ void User::send(std::string)
 {
 
 }
+
+bool User::createRoom(int, std::string, int, int, int)
+{
+
+}
+//done
+bool User::joinRoom(Room* room)
+{
+	if (_currRoom == nullptr)
+	{
+		return(false);
+	}
+	else if (room->joinRoom(this))
+	{
+		_currRoom = room;
+		return(true);
+	}
+	else
+	{
+		return(false);
+	}
+}
+//done
+void User::leaveRoom()
+{
+	if (_currRoom != nullptr)
+	{
+		_currRoom->leaveRoom(this);
+		_currRoom = nullptr;
+	}
+
+}
+//done
+int User::closeRoom()
+{
+	if (_currRoom == nullptr)
+	{
+		return(-1);
+	}
+	int roomId = _currRoom->closeRoom(this);
+	return(roomId);
+}
+//done
+bool User::leaveGame()
+{
+	if (_currGame != nullptr)
+	{
+		_currGame->leaveGame(this);
+		_currGame = nullptr;
+	}
+}
 //done
 void User::setGame(Game* game)
 {
@@ -22,53 +73,23 @@ void User::clearGame()
 	_currGame = nullptr;
 }
 
-bool User::createRoom(int, std::string, int, int, int)
-{
-
-}
-
-bool User::joinRoom(Room*)
-{
-
-}
-
-void User::leaveRoom()
-{
-	if (_currRoom != nullptr)
-	{
-		_currRoom->leaveRoom(this);
-	}
-
-}
-
-int User::closeRoom()
-{
-
-}
-
-bool User::leaveGame()
-{
-
-}
-
-
 //getter
 
 std::string User::getUsername()
 {
 	return(_username);
 }
-
+//done
 tcp::socket* User::getSocket()
 {
 	return(_socket);
 }
-
+//done
 Room* User::getRoom()
 {
 	return(_currRoom);
 }
-
+//done
 Game* User::getGame()
 {
 	return(_currGame);
