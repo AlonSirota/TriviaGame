b9ee2ef6ef90f5492c65ad15c06a698043175559
Eran Yom-Tov <Eran Yom-Tov>
2016-05-18 12:17:21 +0300

    update skeleton file for Room class

--- client/server/Room.h
+++ client/server/Room.h
@@ -1,7 +1,17 @@
-
+#include <vector>
 
 
 class Room
 {
+	Room(int, User*, std::string, int, int, int);
+	bool joinRoom(User*);
+	void leaveRoom(User*);
+	int closeRoom(User*);
+	std::vector<User*> getUsers();
+	std::string getUsersListMessage();
+	int getQuestionsNo();
+	int getId();
+	std::string getName();
 
+	//add private functions and variables
 };
