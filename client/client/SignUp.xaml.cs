2cb3e860467fff449ea3365dd074b5f4f6a1dab8
alonsi <sirota.alon@gmail.com>
2016-05-19 20:12:09 +0300

    added sign up feature, fixed compilation error in myTcpClient

--- client/client/SignUp.xaml.cs
+++ client/client/SignUp.xaml.cs
@@ -19,9 +19,69 @@ namespace client
     /// </summary>
     public partial class SignUp : Window
     {
-        public SignUp()
+        myTcpClient _client;
+        public SignUp(string username, string password, myTcpClient newClient)
         {
             InitializeComponent();
+            this.txtUsername.Text = username;
+            this.txtPassword.Text = password;
+            _client = newClient;
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSignUp_Click(object sender, RoutedEventArgs e)
+        {
+            requestSignUpAsync();
+            lblStatus.Content = "trying to sign up";
+        }
+
+        private async void requestSignUpAsync()
+        {
+            string response = await Task.Factory.StartNew(() => requestSignUp());
+
+            if (response == "1040")
+            {
+                lblStatus.Content = "sign up succeeded";
+            }
+            else if (response == "1041")
+            {
+                lblStatus.Content = "sign up failed: invalid password";
+            }
+            else if (response == "1042")
+            {
+                lblStatus.Content = "sign up failed: username taken";
+            }
+            else if (response == "1043")
+            {
+                lblStatus.Content = "sign up failed: invalid username";
+            }
+            else if (response == "1044")
+            {
+                lblStatus.Content = "sign up failed: other";
+            }
+            else
+            {
+                lblStatus.Content = "error";
+            }
+        }
+
+        //203##username##password##email
+        private string requestSignUp()
+        {
+            _client.mySend(
+                "203" +
+                txtUsername.Text.Length.ToString().PadLeft(2, '0') +
+                txtUsername.Text +
+                txtPassword.Text.Length.ToString().PadLeft(2, '0') +
+                txtPassword.Text +
+                txtEmail.Text.Length.ToString().PadLeft(2, '0') +
+                txtEmail.Text);
+
+            return _client.myReceive(4);
         }
     }
 }
