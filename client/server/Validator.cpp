7450a75a99f136e498f459de27855825c850e465
alonsi <sirota.alon@gmail.com>
2016-05-19 21:27:09 +0300

    started working on validator

--- client/server/Validator.cpp
+++ client/server/Validator.cpp
@@ -1,8 +1,18 @@
 #include "Validator.h"
 
-static bool isPasswordValid(std::string)
+static bool isPasswordValid(std::string pass)
 {
+	std::regex reg = std::regex();
 
+	if (pass.length() < 4) //less than 4 chars
+	{
+		return false;
+	}
+
+	if (pass.find(" ") != -1) //contains spaces
+	{
+		return false;
+	}
 }
 
 static bool isUsernameValid(std::string)
