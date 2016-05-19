fd2c0e3dadc19941f4bca82a6ff8bec71e4d8d13
Eran Yom-Tov <gri120@walla.com>
2016-05-19 20:19:18 +0300

    solved LINK errors in adding boost library, and solved bug in Helper class

---
+++ client/server/Room.cpp
@@ -0,0 +1,19 @@
+#include "Room.h"
+
+Room::Room(int, User *, std::string, int, int, int)
+{
+}
+
+bool Room::joinRoom(User *)
+{
+	return false;
+}
+
+void Room::leaveRoom(User *)
+{
+}
+
+int Room::closeRoom(User *)
+{
+	return 0;
+}
