3505f87cf4bb963ccbc5566945f1f38d454d6250
alonsi <sirota.alon@gmail.com>
2016-06-08 17:25:05 +0300

    clientHandler now only makes a temp user if the socket is new

--- client/server/TriviaServer.cpp
+++ client/server/TriviaServer.cpp
@@ -286,10 +286,14 @@ void TriviaServer::clientHandler(std::shared_ptr<tcp::socket> s)
 	int msgCode = Helper::getMessageTypeCode(s);
 	while (msgCode != 0 && msgCode != EXIT)
 	{
-		std::string newName = "temp_name";
-		newName.append(std::to_string(_tempUserSequence++));
-		std::shared_ptr<User> newUser(new User(newName, s)); //creates a new user
-		_connectedUsers[newUser] = s; //inserts it.
+		if (getUserBySocket(s) == nullptr) //this socket doesn't have User (yet).
+		{
+			//creates it
+			std::string newName = "temp_name";
+			newName.append(std::to_string(_tempUserSequence++));
+			std::shared_ptr<User> newUser(new User(newName, s)); //creates a new user
+			_connectedUsers[newUser] = s; //inserts it.
+		}		
 
 		addRecievedMessage(buildRecievedMessage(s, msgCode));
 		msgCode = Helper::getMessageTypeCode(s);
