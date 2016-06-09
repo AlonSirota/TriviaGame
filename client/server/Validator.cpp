51ef94f47b09c0ca49030bc5fb7298a52da39629
alonsi <sirota.alon@gmail.com>
2016-06-09 12:21:21 +0300

    debugged Validator.cpp

--- client/server/Validator.cpp
+++ client/server/Validator.cpp
@@ -2,25 +2,23 @@
 
 bool Validator::isPasswordValid(std::string pass)
 {
-	if (pass.length() < 4 ||
-		pass.find(" ") != -1 ||
-		std::regex_match(pass, std::regex("[0-9]")) == 0 ||
-		std::regex_match(pass, std::regex("[a-z]")) == 0 ||
-		std::regex_match(pass, std::regex("[A-Z]")) == 0 )
-	{
-		return false;
-	}
-	else
-	{
-		return true;
-	}
+	bool shorterThan4 = pass.length() < 4,
+		containsSpace = pass.find(" ") != -1,
+		containsNumber = std::regex_match(pass, std::regex("[0-9]")),
+		containsLowerCase = std::regex_match(pass, std::regex("[a-z]")),
+		containsUpperCase = std::regex_match(pass, std::regex("[A-Z]"));
+
+	if (shorterThan4 || containsSpace || !containsNumber || !containsLowerCase || !containsUpperCase) return false;
+	else return true;
 }
 
 bool Validator::isUsernameValid(std::string user)
 {
-	if (std::regex_match(user, std::regex("^[a-z|A-Z]")) == 0 ||
-		std::regex_match(user, std::regex(" ")) != 0 ||
-		user == "")
+	bool startsWithLetter = std::regex_search(user, std::regex("^[a-z|A-Z]")),
+		containsSpace = std::regex_match(user, std::regex(" ")),
+		isEmpty = user == "";
+
+	if (!startsWithLetter || containsSpace || isEmpty)
 	{
 		return false;
 	}
