5c63e5a87f173f75fdc95402c9fe0f2933189e9d
alonsi <sirota.alon@gmail.com>
2016-06-13 19:30:21 +0300

    deleted some comments

--- client/server/Source.cpp
+++ client/server/Source.cpp
@@ -7,10 +7,6 @@ int main()
 {
 	TriviaServer server;
 	server.serve();
-	//DB db;
-	//db.addNewUser("user", "Aa12", "debug@help.help");
-	//db.insertQuestion("what color is the sky?", "blue", "red", "green", "yellow");
-	//std::cout << "should print 0: " << db.addAnswerToUser(db.insertNewGame(), "alon", 1, "ans1", true, 3);
 
 	system("pause");
 	return(0);
