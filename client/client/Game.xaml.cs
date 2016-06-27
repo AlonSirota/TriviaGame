8b3650791a08ecbd0e276863f78161f1a45c2cb2
alonsi <sirota.alon@gmail.com>
2016-06-27 15:38:35 +0300

    renamed variables of Game. setting up the timer in a seperate function.

--- client/client/Game.xaml.cs
+++ client/client/Game.xaml.cs
@@ -12,18 +12,18 @@ namespace client
     public partial class Game : Window
     {
         myTcpClient _client;
-        int ansNo = 0;
-        int time;
-        string question;
-        string ans1;
-        string ans2;
-        string ans3;
-        string ans4;
-        int timePerQuestion;
-        DispatcherTimer dispatcherTimer;
+        int _ansIndex = 0;
+        int _time;
+        string _question;
+        string _ans1;
+        string _ans2;
+        string _ans3;
+        string _ans4;
+        int _timePerQuestion;
+        DispatcherTimer _dispatcherTimer;
 
-        int correctNo = 0;
-        int questionNo = 0;
+        int _correctAnswerIndex = 0;
+        int _currentQuestionIndex = 0;
         //for score
         List<string> _userList = new List<string>();
         List<int> _scores = new List<int>();
@@ -37,18 +37,23 @@ namespace client
         {
             InitializeComponent();
             _client = newClient;
-            timePerQuestion = time;
-            dispatcherTimer = new DispatcherTimer();
-            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            _timePerQuestion = time;
+            setUpTimer();
             getQuestionFirst();
             asignAnswers();
         }
 
+        public void setUpTimer()
+        {
+            _dispatcherTimer = new DispatcherTimer();
+            _dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            _dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+        }
+
         private void btnAns1_Click(object sender, RoutedEventArgs e)
         {
-            ansNo = 1;
-            dispatcherTimer.Stop();
+            _ansIndex = 1;
+            _dispatcherTimer.Stop();
             sendAns();
         }
         private async void sendAns()
@@ -59,12 +64,12 @@ namespace client
             {
                 lblStatus.Content = "Correct code detected";
                 //indicate if correct
-                questionNo++;
+                _currentQuestionIndex++;
                 if (response[3] == '1')
                 {
-                    correctNo++;
+                    _correctAnswerIndex++;
                 }
-                lblScore.Content = correctNo.ToString() + "/" + questionNo.ToString();
+                lblScore.Content = _correctAnswerIndex.ToString() + "/" + _currentQuestionIndex.ToString();
                 //get question
                 response = await Task.Factory.StartNew(() => getQuestion());
                 code = response.Substring(0, 3);
@@ -101,7 +106,7 @@ namespace client
 
         private string sendAnswer()
         {
-            string message = "219" + ansNo.ToString() + ((int)time).ToString().PadLeft(2, '0');
+            string message = "219" + _ansIndex.ToString() + ((int)_time).ToString().PadLeft(2, '0');
             _client.mySend(message);
             string code = _client.myReceive(3);
             string ans = "";
@@ -118,15 +123,15 @@ namespace client
             if(code == "118")
             {
                 int stringSize = Int32.Parse(_client.myReceive(3));
-                question = _client.myReceive(stringSize);
+                _question = _client.myReceive(stringSize);
                 stringSize = Int32.Parse(_client.myReceive(3));
-                ans1 = _client.myReceive(stringSize);
+                _ans1 = _client.myReceive(stringSize);
                 stringSize = Int32.Parse(_client.myReceive(3));
-                ans2 = _client.myReceive(stringSize);
+                _ans2 = _client.myReceive(stringSize);
                 stringSize = Int32.Parse(_client.myReceive(3));
-                ans3 = _client.myReceive(stringSize);
+                _ans3 = _client.myReceive(stringSize);
                 stringSize = Int32.Parse(_client.myReceive(3));
-                ans4 = _client.myReceive(stringSize);
+                _ans4 = _client.myReceive(stringSize);
             }
             else if(code == "121")
             {
@@ -147,60 +152,60 @@ namespace client
         {
     
             int stringSize = Int32.Parse(_client.myReceive(3));
-            question = _client.myReceive(stringSize);
+            _question = _client.myReceive(stringSize);
             stringSize = Int32.Parse(_client.myReceive(3));
-            ans1 = _client.myReceive(stringSize);
+            _ans1 = _client.myReceive(stringSize);
             stringSize = Int32.Parse(_client.myReceive(3));
-            ans2 = _client.myReceive(stringSize);
+            _ans2 = _client.myReceive(stringSize);
             stringSize = Int32.Parse(_client.myReceive(3));
-            ans3 = _client.myReceive(stringSize);
+            _ans3 = _client.myReceive(stringSize);
             stringSize = Int32.Parse(_client.myReceive(3));
-            ans4 = _client.myReceive(stringSize);
+            _ans4 = _client.myReceive(stringSize);
         }
 
         private void asignAnswers()
         {
-            btnAns1.Content = ans1;
-            btnAns2.Content = ans2;
-            btnAns3.Content = ans3;
-            btnAns4.Content = ans4;
-            lblQuestion.Content = question;
+            btnAns1.Content = _ans1;
+            btnAns2.Content = _ans2;
+            btnAns3.Content = _ans3;
+            btnAns4.Content = _ans4;
+            lblQuestion.Content = _question;
             //timer
-            time = timePerQuestion;
-            dispatcherTimer.Start();
+            _time = _timePerQuestion;
+            _dispatcherTimer.Start();
         }
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
-            time--;
-            if(time != 0)
+            _time--;
+            if(_time != 0)
             {
-                lblTime.Content = time;
+                lblTime.Content = _time;
             }
             else
             {
-                ansNo = 5;
+                _ansIndex = 5;
                 sendAns();
             }
         }
 
         private void btnAns2_Click(object sender, RoutedEventArgs e)
         {
-            ansNo = 2;
-            dispatcherTimer.Stop();
+            _ansIndex = 2;
+            _dispatcherTimer.Stop();
             sendAns();
         }
 
         private void btnAns3_Click(object sender, RoutedEventArgs e)
         {
-            ansNo = 3;
-            dispatcherTimer.Stop();
+            _ansIndex = 3;
+            _dispatcherTimer.Stop();
             sendAns();
         }
 
         private void btnAns4_Click(object sender, RoutedEventArgs e)
         {
-            ansNo = 4;
-            dispatcherTimer.Stop();
+            _ansIndex = 4;
+            _dispatcherTimer.Stop();
             sendAns();
         }
 
